604e501c759ba8bd74da7b738a50c05b6b636421
altamash00777 <altamasn186@gmail.com>
2025-12-30 11:06:35 +0530

    evensum

---
+++ evensum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[]={2,3,5,7,8,4,2};
+    int size=sizeof(arr)/sizeof(arr[0]);
+
+    int sum=0;
+
+    for(int i=0;i<size;i++){
+        if(arr[i]%2==0){
+        sum=sum+arr[i];        
+        }
+    }
+    cout<<sum;
+
+
+
+
+
+
+    return 0;
+}
